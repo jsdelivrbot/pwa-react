52597ca5585de96b0bb159efc27c5606a5e68ba8
rbalaj <renaldobalaj@gmail.com>
2018-07-19 02:18:48 +0200

    indexdb

--- src/index.js
+++ src/index.js
@@ -2,75 +2,38 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Script from './script.js';
+import json1 from './products.json';
 import registerServiceWorker from './registerServiceWorker';
 
-var products =[
-{
-	id:1, 
-	name:"Laptop USB FAN", 
-	price:3.29 ,
-	src: "https://i.ebayimg.com/images/g/SfQAAOSwbtNaBnS3/s-l1600.jpg"
+var data = require('./products.json'); // forward slashes will depend on the file location
+console.log(data);
+window.myArr =[];
 
-},
-{
-	id:2, 
-	name:"I am Groot T-shirt", 
-	price:8.89 ,
-	src: "https://i.ebayimg.com/images/g/HXoAAOSwhzRaWFA5/s-l1600.jpg"
-},
-{
-	id:3, 
-	name:"Samsung s8 I am groot case", 
-	price:3.99 ,
-	src: "https://i.ebayimg.com/images/g/xp8AAOSwxphbId4S/s-l1600.jpg"
-},
-{
-	id:4, 
-	name:"I am Groot toy", 
-	price:3.29 ,
-	src: "https://i.ebayimg.com/images/g/ZpMAAOSw7GRZHVFw/s-l1600.jpg"
-},
-{
-	id:5, 
-	name:"UGREEN Quick Charge 3.0", 
-	price:13.29 ,
-	src: "https://i.ebayimg.com/images/g/HWIAAOSwSeVaLj3M/s-l1600.jpg"
 
-},
-{
-	id:6, 
-	name:"Bluetooth Car Charger + MP3", 
-	price:8.89 ,
-	src: "https://i.ebayimg.com/images/g/R2oAAOSwh5hbC-sB/s-l500.jpg"
-},
-{
-	id:7, 
-	name:"Fujifilm Instax Instant Camera", 
-	price:88.19 ,
-	src: "https://i.ebayimg.com/images/g/ZTUAAOSwuINbRcx-/s-l1600.jpg"
-},
-{
-	id:8, 
-	name:"ZOSI Security Camera System", 
-	price:3.29 ,
-	src: "https://i.ebayimg.com/images/g/2zEAAOSwZrdbRKqN/s-l1600.jpg"
-}
-]
+//         const script = document.createElement("script");
+
+//         script.src = {Script};
+//         script.async = true;
+
+//         document.body.appendChild(script);
+    
+
 class Main extends Component {
+//react router 
+render() {
 
-	render() {
-
-		return (
-			<div>
-			<App products={this.props.products}/> 
-			</div>
-			);
-		}
-	}
+	return (
+		<div>
+		<App products={this.props.products}/> 
+		</div>
+		);
+}
+}
 
 
-	ReactDOM.render( <Main products={products} />
+ReactDOM.render( <Main products={window.myArr} />
 	, document.getElementById('root'));
 
 
-	registerServiceWorker();
+registerServiceWorker();
